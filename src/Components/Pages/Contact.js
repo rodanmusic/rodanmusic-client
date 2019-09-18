8e31c1691279d5c5292561a884c7b7b064b2d2d2
rodanmusic <rodanmusic@icloud.com>
2019-09-18 05:03:43 +0000

    Updating axios request bug

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -42,7 +42,6 @@ export default (props) => {
         resetStatusMessages();
         axios.post(CONTACT_ENDPOINT, {
             method: 'post',
-            url: CONTACT_ENDPOINT,
             data: {
                 name: messageInput.name,
                 email: messageInput.email,
