6d8350c188a8acb916025e06f4986c2236919656
Norm Johnson <rodanmusic@Normans-iMac.domain>
2020-04-16 14:57:48 -0600

    Updating events page to include the C0LDSTORAGE twitch stream.

--- src/Components/Pages/Events.js
+++ src/Components/Pages/Events.js
@@ -5,9 +5,19 @@ import { ContentContainer } from '../Layout'
 export default (props) => {
     return (
         <ContentContainer title='EVENTS' >
-            <Typography paragraph variant='body2'>
-                No Events Currently
+            <Typography paragraph variant='caption'>
+                Rodan Streams live on the <a href='https://www.twitch.tv/c0ldstorage?tt_content=text_link&tt_medium=live_embed'>C0LDSTORAGE</a> with Soda Pop every Saturday @ 9PM MST.
             </Typography>
+            <iframe 
+                src='https://player.twitch.tv/?channel=c0ldstorage'
+                style={{width: '100%', height: '300px'}}
+                title='COLDSTORAGE Stream'
+                frameborder='0'
+                allowfullscreen='true' 
+                scrolling='no'
+                height='378' 
+                width='620'>
+            </iframe>                
         </ContentContainer>
     );
 }
