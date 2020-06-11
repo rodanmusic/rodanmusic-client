b1523d49cef4a3ebc4428b77d72ba1b6151fd9e0
Norm Johnson <rodanmusic@icloud.com>
2020-06-11 13:07:12 -0600

    Updating Twitch iframe with parent parameter.
    Twitch changed required query parameters for embedding an iframe

--- src/Components/Pages/Events.js
+++ src/Components/Pages/Events.js
@@ -9,7 +9,7 @@ export default (props) => {
                 Rodan Streams live on the <a href='https://www.twitch.tv/c0ldstorage?tt_content=text_link&tt_medium=live_embed'>C0LDSTORAGE</a> with Soda Pop every Saturday @ 9PM MST.
             </Typography>
             <iframe 
-                src='https://player.twitch.tv/?channel=c0ldstorage'
+                src='https://player.twitch.tv/?channel=c0ldstorage&parent=www.rodanmusic.com'
                 style={{width: '100%', height: '300px'}}
                 title='COLDSTORAGE Stream'
                 frameBorder='0'
