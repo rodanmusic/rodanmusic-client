4027ab8b115a8d93c23b41e518178847a2c44585
Norm Johnson <rodanmusic@Normans-iMac.domain>
2020-07-26 19:17:57 -0600

    Removing paradigm festival

--- src/Components/Pages/Events.js
+++ src/Components/Pages/Events.js
@@ -7,18 +7,6 @@ export default (props) => {
     const imageSize = {width: '95%', height: '95%'};
     return (
         <>
-            <ContentContainer title='SHOWS' >
-                <div align='center'>
-                    <a href='https://www.facebook.com/events/197435258296111/'>
-                        <img style={imageSize} src={Paradigm} alt='Rodan' />
-                    </a>
-                </div>
-                <div align='center'>
-                    <Typography paragraph variant='subtitle2'>
-                        <a href='https://www.facebook.com/events/197435258296111/'>More Info Here</a>
-                    </Typography>       
-                </div>     
-            </ContentContainer>
             <ContentContainer title='LIVE STREAM'>
                 <Typography paragraph variant='caption'>
                     Rodan Streams live on the <a href='https://www.twitch.tv/c0ldstorage?tt_content=text_link&tt_medium=live_embed'>c0ldstorage</a> with Soda Pop every Saturday @ 9PM MST.
