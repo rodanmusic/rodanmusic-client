a3a0e13fb23739340b81d0159cdfbaf3d65a5515
Norman L Johnson III <rodanmusic@Normans-iMac.domain>
2021-07-07 21:50:02 -0600

    Adding limbleshwiften event

--- src/Components/Pages/Events.js
+++ src/Components/Pages/Events.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Typography} from '@material-ui/core';
 import { ContentContainer } from '../Layout'
+import limb from '../Pages/Events/limb.jpg';
+
+const styles = {
+    image: {
+      width: '100%'
+    }
+};
 
 export default (props) => {
     return (
         <>
-            <ContentContainer title='LIVE STREAM'>
+            <ContentContainer title='c0ldstorage live'>
                 <Typography paragraph variant='caption'>
                     Rodan Streams live on the <a href='https://www.twitch.tv/c0ldstorage?tt_content=text_link&tt_medium=live_embed'>c0ldstorage</a> with Soda Pop every Saturday @ 9PM MST.
                 </Typography>
@@ -20,6 +27,14 @@ export default (props) => {
                     width='620'>
                 </iframe>                
             </ContentContainer>        
+            <ContentContainer title='08-06-2021 - Limbleshwiften Presents'>
+                <Typography paragraph variant='caption'>
+                    A Night of House & Techno featuring c0ldstorage.
+                </Typography>
+                    <a href="https://www.facebook.com/events/350740229981955/?ref=newsfeed">
+                        <img style={styles.image} src={limb} alt='Limbleshwiften Presents' />
+                    </a>
+            </ContentContainer>               
         </>
     );
 }
